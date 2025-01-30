7f9515f8db1a1bf51545e58936485e476a032bd8
Greeshma2903 <70336930+Greeshma2903@users.noreply.github.com>
2025-01-30 18:05:41 +0530

    fix: Update map iframe

--- components/ContactFooter/ContactFooter.tsx
+++ components/ContactFooter/ContactFooter.tsx
@@ -93,8 +93,8 @@ const ContactFooter = () => {
           {/* right section */}
           <Stack className={classes.rightSection} gap={20}>
             <iframe
+              src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3844.809941874043!2d73.825256!3d15.494652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTXCsDI5JzQwLjgiTiA3M8KwNDknMzAuOSJF!5e0!3m2!1sen!2sin!4v1738240409189!5m2!1sen!2sin"
               title="Our address on Google Maps"
-              src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3844.721290839657!2d73.8286111!3d15.499416700000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTXCsDI5JzU3LjkiTiA3M8KwNDknNDMuMCJF!5e0!3m2!1sen!2sin!4v1734200907821!5m2!1sen!2sin"
               style={{ border: 0, width: "100%", height: "100%" }}
               loading="lazy"
             ></iframe>
--- lib/data/constants.ts
+++ lib/data/constants.ts
@@ -1,5 +1,5 @@
 export const CONTACT_NUMBER = "7028161277";
 export const CONTACT_URL = `https://api.whatsapp.com/send?phone=91${CONTACT_NUMBER}`;
-export const MAP_URL = "https://maps.app.goo.gl/1Hg2QDyMMwdmS9qw5";
+export const MAP_URL = "https://maps.app.goo.gl/X5kcUQ5WTb1xSKbb9";
 export const LINKEDIN_URL = "https://www.linkedin.com/company/big-brother-goa/";
 export const INSTAGRAM_URL = "https://www.instagram.com/bigbrother.in/";
